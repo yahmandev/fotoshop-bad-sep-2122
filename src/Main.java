5723c96aa23f0becaff7909ebabd6c3a4d9aa02c
unknown <furqan1934@gmail.com>
2022-03-03 04:01:32 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * This is the main class for the Fotoshop application
+ * 
+ * @author Joseph Williams
+ * @version 09.01.2022
+ */
+public class Main {
+   public static void main(String[] args) {
+
+       Editor editor = new Editor(null, null, null, null);
+       editor.set();
+       editor.edit();
+    }
+}
